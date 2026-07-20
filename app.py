a2ead0df5f4128973dcedb5a59d86e2a46feaf10
sameraishi460-cmd <sameraishi460@gmail.com>
2026-07-20 16:03:14 +0300

    Correct typo in import statement
    Fix import statement by correcting a typo in 'session'.

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from pathlib import Path
-
-code = """from flask import Flask, render_template, request, redirect, session
+ """from flask import Flask, render_template, request, redirect, sessio
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
